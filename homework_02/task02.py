ffa0023906159d7119c6b1c92258552b42e64e8f
Liubov Elkhovskaya <36673448+Siella@users.noreply.github.com>
2021-11-10 17:14:34 +0300

    Update task02.py
    fixes for black and isort

--- homework_02/task02.py
+++ homework_02/task02.py
@@ -1,7 +1,7 @@
 """
 Given a cell with "it's a fib sequence" from slideshow,
-    please write function "check_fib", which accepts a Sequence of integers, and
-    returns if the given sequence is a Fibonacci sequence
+    please write function "check_fib", which accepts a Sequence of integers,
+    and returns if the given sequence is a Fibonacci sequence
 
 We guarantee, that the given sequence contain >= 0 integers inside.
 
@@ -13,11 +13,11 @@ def check_fib(data: Sequence[int]) -> bool:
     if len(data) < 2:
         print('A sequence should have at least two elements!')
         return False
-    for n,f_n in enumerate(data[:-2]):
+    for n, f_n in enumerate(data[:-2]):
         f_n1 = data[n+1]
         f_n2 = data[n+2]
         if not (f_n == f_n2 - f_n1):
-            print( "It's NOT a fib sequence!")
+            print("It's NOT a fib sequence!")
             return False
     print("It's a fib sequence!")
     return True
