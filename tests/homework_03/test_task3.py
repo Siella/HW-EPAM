6a42e1c4a4ee0b18333c0a86aabbf9b0402ef3ae
Liubov Elkhovskaya <36673448+Siella@users.noreply.github.com>
2021-11-10 18:55:12 +0300

    Update test_task3.py

--- tests/homework_03/test_task3.py
+++ tests/homework_03/test_task3.py
@@ -4,10 +4,16 @@ from homework_03.task03 import find_maximum_and_minimum
 
 
 def test_find_maximum_and_minimum():
-    PATH = os.getcwd()
-    assert find_maximum_and_minimum(PATH+'/test1.txt') == (-1, -1), \
+    def find(name, path):
+        for root, dirs, files in os.walk(path):
+            if name in files:
+                return os.path.join(root, name)
+    test_file1 = find('test1.txt', os.getcwd())
+    test_file2 = find('test2.txt', os.getcwd())
+    test_file3 = find('test3.txt', os.getcwd())
+    assert find_maximum_and_minimum(test_file1) == (-1, -1), \
         'Failed the 1st case.'
-    assert find_maximum_and_minimum(PATH+'/test2.txt') == (1, 1), \
+    assert find_maximum_and_minimum(test_file2) == (1, 1), \
         'Failed the 2nd case.'
-    assert find_maximum_and_minimum(PATH+'/test3.txt') == (1, 5), \
+    assert find_maximum_and_minimum(test_file3) == (1, 5), \
         'Failed the 3rd case.'
