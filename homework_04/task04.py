92d692b061cc95ef910d62e6b3b38207d6ffa6f1
Liubov Elkhovskaya <36673448+Siella@users.noreply.github.com>
2021-11-10 16:55:27 +0300

    task_4 submission + tests

---
+++ homework_04/task04.py
@@ -0,0 +1,26 @@
+"""
+Classic task, a kind of walnut for you
+
+Given four lists A, B, C, D of integer values,
+    compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
+
+We guarantee, that all A, B, C, D have same length of N where 0 ≤ N ≤ 1000.
+"""
+from typing import List
+
+import numpy as np
+import itertools
+
+
+def check_sum_of_four(a: List[int], b: List[int], c: List[int], d: List[int]) -> int:
+    K, N = 4, len(a)
+    matrix = np.array([a, b, c, d])
+    cnt_tuples = 0
+    for i in itertools.product([0, 1], repeat=K * N):
+        ones_matrix = np.reshape(np.array(i), (N, K))
+        if all(ones_matrix.sum(axis=0) == np.array([1,1,1,1])):
+            if np.matmul(matrix, ones_matrix).trace() == 0:
+                cnt_tuples += 1
+    return cnt_tuples
+
+
---
+++ tests/homework_04/test_task4.py
@@ -0,0 +1,9 @@
+from homework_04.task04 import check_sum_of_four
+
+
+def test_check_sum_of_four():
+    assert check_sum_of_four([], [], [], []) == 0, 'Failed the 1st case.'
+    assert check_sum_of_four([1], [2], [3], [4]) == 0, 'Failed the 2nd case.'
+    assert check_sum_of_four([1], [-1], [0], [0]) == 1, 'Failed the 3rd case.'
+    assert check_sum_of_four([1, 2], [-1, 0], [5, 23], [6, -23]) == 1, 'Failed the 4th case.'
+    assert check_sum_of_four([1, 2, 3], [-1, 0, 3], [5, 23, -3], [6, -23, 3]) == 2, 'Failed the 5th case.'
