552a9f05aa1751b60f7278dbf9a0d6ee18c381bd
Liubov Elkhovskaya <36673448+Siella@users.noreply.github.com>
2021-11-14 01:23:00 +0300

    Update tests_01.py

--- tests/homework_01/tests_01.py
+++ tests/homework_01/tests_01.py
@@ -5,17 +5,33 @@ from homework_01.hw1 import (count_non_ascii_chars, count_punctuation_chars,
                              get_most_common_non_ascii_char, get_rarest_char)
 
 
-def test_sample():
-    def find(name, path):
-        for root, dirs, files in os.walk(path):
-            if name in files:
-                return os.path.join(root, name)
-    sample = find('par_1.txt', os.getcwd())
+def find(name, path):
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            return os.path.join(root, name)
+
+
+sample = find('par_1.txt', os.getcwd())
+
+
+def test_get_longest_diverse_words():
     assert set(get_longest_diverse_words(sample)) == {
         'Betrachtung', 'bedenklichen', 'hinausf\u00fchren',
         'vorgebahnte', 'Waldgang', 'vielmehr', 'verbirgt',
         'Ausflug', 'hinter', 'gefa\u00dft'}, 'Fail 1'
+
+
+def test_get_rarest_char():
     assert get_rarest_char(sample) == 'W', 'Fail 2'
+
+
+def test_count_punctuation_chars():
     assert count_punctuation_chars(sample) == 8, 'Fail 3'
+
+
+def test_count_non_ascii_chars():
     assert count_non_ascii_chars(sample) == 6, 'Fail 4'
+
+
+def test_get_most_common_non_ascii_char():
     assert get_most_common_non_ascii_char(sample) == '\u00fc', 'Fail 5'
