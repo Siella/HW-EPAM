8f7f7b8392012f7f8441dbce3fa57c54491934e1
Liubov Elkhovskaya <36673448+Siella@users.noreply.github.com>
2021-11-14 00:53:28 +0300

    Update tests_01.py
    black fixes

--- tests/homework_01/tests_01.py
+++ tests/homework_01/tests_01.py
@@ -16,7 +16,7 @@ def test_sample():
     sample = find('par_1.txt', os.getcwd())
     assert set(get_longest_diverse_words(sample)) == {
         'Betrachtung', 'bedenklichen', 'hinausführen',
-        'vorgebahnte','Waldgang', 'vielmehr', 'verbirgt',
+        'vorgebahnte', 'Waldgang', 'vielmehr', 'verbirgt',
         'Ausflug', 'hinter', 'gefaßt'}, 'Fail 1'
     assert get_rarest_char(sample) == 'W', 'Fail 2'
     assert count_punctuation_chars(sample) == 8, 'Fail 3'
