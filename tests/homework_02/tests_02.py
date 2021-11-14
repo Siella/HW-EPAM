efe88757c7b6ed6b961932ca0f07c31cddf66c23
Liubov Elkhovskaya <36673448+Siella@users.noreply.github.com>
2021-11-15 01:19:05 +0300

    task 2 + tests

--- tests/homework_02/tests_02.py
+++ tests/homework_02/tests_02.py
@@ -1,2 +1,7 @@
-def test_something():
-    pass
+from homework_02.hw2 import major_and_minor_elem
+
+
+def test_major_and_minor_elem():
+    assert major_and_minor_elem([1, 2], [3, 4]) == (3, 2), 'Fail 1'
+    assert major_and_minor_elem([2, 2, 1, 1, 1, 2, 2]) == (2, 1), 'Fail 2'
+    assert major_and_minor_elem([2, 3, 1, 1, 1, 2]) == (1, 3), 'Fail 3'
