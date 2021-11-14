cd4805bd5798b643697112b62dbef7409e82b2e7
Liubov Elkhovskaya <36673448+Siella@users.noreply.github.com>
2021-11-15 02:34:40 +0300

    Update test_02.py
    small fix

--- tests/homework_02/test_02.py
+++ tests/homework_02/test_02.py
@@ -2,6 +2,6 @@ from homework_02.hw2 import major_and_minor_elem
 
 
 def test_major_and_minor_elem():
-    assert major_and_minor_elem([1, 2], [3, 4]) == (3, 2), 'Fail 1'
+    assert major_and_minor_elem([3, 2, 3]) == (3, 2), 'Fail 1'
     assert major_and_minor_elem([2, 2, 1, 1, 1, 2, 2]) == (2, 1), 'Fail 2'
     assert major_and_minor_elem([2, 3, 1, 1, 1, 2]) == (1, 3), 'Fail 3'
