e11ff76d4099a1092ac5c4e53f07730c9659f46b
Liubov Elkhovskaya <36673448+Siella@users.noreply.github.com>
2021-11-21 19:51:25 +0300

    args & kwargs in cache

--- task01/task01.py
+++ task01/task01.py
@@ -7,20 +7,21 @@ def cache(times: int) -> Callable:
     def wrapped_cache(func: Callable) -> Callable:
         global cache_dict
 
-        def access_cache(*args):
-            if args not in cache_dict:
-                cache_dict[args] = (func(*args), -1)
-            func_val, func_times = cache_dict[args]
+        def access_cache(*args, **kwargs):
+            key = str((args, kwargs))
+            if key not in cache_dict:
+                cache_dict[key] = (func(*args, **kwargs), -1)
+            func_val, func_times = cache_dict[key]
             if func_times == times:
-                del cache_dict[args]
-                return func(*args)
+                del cache_dict[key]
+                return func(*args, **kwargs)
             else:
-                cache_dict[args] = (func_val, func_times + 1)
+                cache_dict[key] = (func_val, func_times + 1)
                 return func_val
         return access_cache
     return wrapped_cache
 
 
 @cache(times=2)
-def f():
+def f(some_kwarg=0):
     return input('? ')
