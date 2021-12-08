f7779dcaafcf632efade80504d7bc268d7e29d68
Любовь Елховская <lyubov.elkhovskaya@gmail.com>
2021-12-08 17:16:25 +0300

    delete unnecessary

--- tests/homework6/test_counter.py
+++
@@ -1,9 +0,0 @@
-from homework6.counter import User
-
-
-def test_counter():
-    assert User.get_created_instances() == 0
-    user, _, _ = User(), User(), User()
-    assert user.get_created_instances() == 3
-    assert user.reset_instances_counter() == 3
-    assert user.reset_instances_counter() == 0
